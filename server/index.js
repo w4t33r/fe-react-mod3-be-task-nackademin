25aa009d914a4c4594b46133f1f60d2baa8f3d0c
w44t3r <vk.educate@protonmail.ch>
2022-12-26 16:39:56 +0100

    db + connection to server, index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-
+const sequelize = require('./db')
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const host = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+host()
