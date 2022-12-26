32f65e19106a025e8ce15af7d8c4f65b503323b8
w44t3r <vk.educate@protonmail.ch>
2022-12-26 17:42:04 +0100

    completed server configuration, created db tables and relations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-const PORT = process.env.PORT || 3000
-const app = express()
+const models = require('./models/models')
 
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Working'})
+// })
 
 const host = async () => {
     try {
