e3fa8902e596d697c1fd44a4f5bea3e3ed50af5a
w44t3r <vk.educate@protonmail.ch>
2022-12-26 01:24:10 +0100

    init servere js

---
+++ server/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const express = require('express')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
